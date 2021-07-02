fff8df9e9d5d187ee060d082563933ffac21a710
Yonatan Fessehaye <fessehayeyonatan@gmail.com>
2021-07-03 00:48:17 +0200

    few more eslint config

--- contracts/factory/Factory.sol
+++ contracts/factory/Factory.sol
@@ -1,44 +1,46 @@
 pragma solidity ^0.7.0;
-contract Factory{
-     Child[] public children;
-     uint disabledCount;
 
-    event ChildCreated(address childAddress, uint data);
+contract Factory {
+    Child[] public children;
+    uint256 disabledCount;
 
-     function createChild(uint data) external{
-       Child child = new Child(data, children.length);
-       children.push(child);
-       emit ChildCreated(address(child), data);
-     }
+    event ChildCreated(address childAddress, uint256 data);
 
-     function getChildren() external view returns(Child[] memory _children){
-       _children = new Child[](children.length- disabledCount);
-       uint count;
-       for(uint i=0;i<children.length; i++){
-          if(children[i].isEnabled()){
-             _children[count] = children[i];
-             count++;
-          }
-        }
-     }  
-
-     function disable(Child child) external {
-        children[child.index()].disable();
-        disabledCount++;
-     }
- 
-}
-contract Child{
-    uint data;
-    bool public isEnabled;
-    uint public index;
-    constructor(uint _data,uint _index){
-       data = _data;
-       isEnabled = true;
-       index = _index;
+    function createChild(uint256 data) external {
+        Child child = new Child(data, children.length);
+        children.push(child);
+        emit ChildCreated(address(child), data);
     }
 
-    function disable() external{
-      isEnabled = false;
+    function getChildren() external view returns (Child[] memory _children) {
+        _children = new Child[](children.length - disabledCount);
+        uint256 count;
+        for (uint256 i = 0; i < children.length; i++) {
+            if (children[i].isEnabled()) {
+                _children[count] = children[i];
+                count++;
+            }
+        }
+    }
+
+    function disable(Child child) external {
+        children[child.index()].disable();
+        disabledCount++;
+    }
+}
+
+contract Child {
+    uint256 data;
+    bool public isEnabled;
+    uint256 public index;
+
+    constructor(uint256 _data, uint256 _index) {
+        data = _data;
+        isEnabled = true;
+        index = _index;
+    }
+
+    function disable() external {
+        isEnabled = false;
     }
 }
